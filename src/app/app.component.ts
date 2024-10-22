d1eca12473a8c4e87d21e411e82b9b6ec7836150
Karel Sluka <karel.sluka@gmail.com>
2024-10-22 18:29:21 +0200

    Create tour of hero according to UX design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    HeaderComponent,
+    DashboardComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'AlzaTest';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { CreateHeroComponent } from './components/create-hero/create-hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'/heroes', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'detail/:id', loadComponent: () => import("./components/hero-detail/hero-detail.component").then((m) => m.HeroDetailComponent) },
+    { path: 'create-hero', component: CreateHeroComponent}
+];
